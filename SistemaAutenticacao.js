5415ee577dab37663324ab0b2bb063513b5caae1
keilacardodo <keila_dcardoso@hotmail.com>
2021-09-01 10:57:01 -0300

    autenticacao

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -4,6 +4,13 @@ Ser autenticável significa ter o metodo autenticar
 
 export class SistemaAutenticacao {
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.autencicacao(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static autencicacao(autenticavel){
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-import { Cliente } from "./Cliente.js";
-import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca} from "./ContaPoupanca.js";
+import {Cliente} from "./Cliente.js";
+import {Gerente} from "./Funcionarios/Gerente.js";
+import {Diretor} from "./Funcionarios/Diretor.js";
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js";
 
-const cliente1 = new Cliente("Keila Cardoso", 47654070802);
-const cliente2 = new Cliente("Leandro Cardoso", 15649965811);
-
-const cc1 = new ContaCorrente(1001, cliente1);
-cc1.depositar(5000);
-cc1.sacar(200);
-
-const cc2 = new ContaPoupanca(50,1001, cliente2);
-
-console.log(cc1);
-console.log(cc2);
-console.log("Contas: " + ContaCorrente.numeroContas);
+const diretor = new Diretor("Rodrigo", 10000, 1234567900);
+const gerente = new Gerente("Ricardo", 5000, 12345879920);
 
 
+gerente.cadastrarSenha("123");
+const login = SistemaAutenticacao.login(gerente, "123");
 
+console.log(login)
